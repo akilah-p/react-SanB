ec9b3cc67f480d4037dc1535ed2eec468be6a267
Akilah Powell <atpjun@gmail.com>
2023-01-19 10:42:18 -0800

    created pages

---
+++ src/components/Music-Player/Music.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class CardProfile extends React.Component {
+    state = {
+        index: 3,
+        currentTime: '0:00',
+        musicList: [{name: 'Aguacero', artist: 'Bad Bunny', img:}]
+    }
+}
