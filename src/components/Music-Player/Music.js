99e037de478124d09631ce070b04d6cd2d278ae3
Akilah Powell <atpjun@gmail.com>
2023-01-19 10:47:58 -0800

    trying to create remote branch

--- src/components/Music-Player/Music.js
+++ src/components/Music-Player/Music.js
@@ -4,6 +4,6 @@ class CardProfile extends React.Component {
     state = {
         index: 3,
         currentTime: '0:00',
-        musicList: [{name: 'Aguacero', artist: 'Bad Bunny', img:}]
+        musicList: [{name: 'Aguacero', artist: 'Bad Bunny', img:'https://media.giphy.com/media/DD4FroTT30PeSamZbG/giphy.gif'}]
     }
 }
