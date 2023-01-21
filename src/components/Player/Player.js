0d6d3fd85ca1834f09a7350104448b8d370c2dab
Akilah Powell <atpjun@gmail.com>
2023-01-20 18:23:06 -0800

    added dependecies now creating player

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,1 +1,10 @@
 import { useEffect, useState } from 'react';
+import useSound from 'use-sound';
+import { AiFillPlayCircle, AiFillPauseCircle } from 'react-icons/ai';
+
+import { BiSkipNext, BiSkipPrevious } from 'react-icons/bi';
+import { IconContext} from 'react-icons';
+
+
+const [isPlaying, setIsPlaying] = useState(false);
+const [play, {pause, duration, sound}] = useSound()
